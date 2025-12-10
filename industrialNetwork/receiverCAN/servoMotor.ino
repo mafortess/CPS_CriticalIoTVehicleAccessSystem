180a3c4f281a1ef51cb14a983f32a536636e874b
Diego Arroyo <diegodiegito6@gmail.com>
2025-12-10 20:14:37 +0100

    Added sensor code for gas and air quality sensors

---
+++ industrialNetwork/receiverCAN/servoMotor.ino
@@ -0,0 +1,93 @@
+#include <SPI.h>
+//#include <mcp_can.h>
+#include <mcp2515.h>
+#include "Air_Quality_Sensor.h"
+#include <ESP32Servo.h>
+
+
+#define CAN_CS 5  // Chip Select pin
+struct MCP2515 mcp2515(5); // CS pin is GPIO 5
+#define SERVO_PIN 26 // ESP32 pin GPIO26 connected to servo motor
+
+Servo servoMotor;
+
+#define SERVO_ID_IN 0x321 // Will always receive 0 or 1
+struct can_frame canMsgReceived;
+
+#define SERVO_ID_OUT 0x421 // Will send barrier degrees
+struct can_frame canMsgSent;
+
+#define BARRIER_TIMEOUT_MS 5000
+
+int lastOrderTimestamp = 0;
+int verbose = 1;
+
+int servoAngle = 0;
+int servoOrder = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+
+  SPI.begin();
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+
+  servoMotor.attach(SERVO_PIN);  // attaches the servo on ESP32 pin
+}
+
+void loop() {
+  
+
+  if (millis() - lastOrderTimestamp > BARRIER_TIMEOUT_MS) servoOrder = 0;
+
+  if (mcp2515.readMessage(&canMsgReceived) == MCP2515::ERROR_OK)
+  {
+    if (canMsgReceived.can_id == SERVO_ID_IN)  // Check if the message is from the sender
+    {
+      
+      if (canMsgReceived.data[0] == 0) {
+        servoOrder = 0;
+      }
+      else {
+        servoOrder = 1;
+        lastOrderTimestamp = millis();
+      }
+
+      if (verbose) {
+        Serial.print("Message received: ");
+        Serial.println(canMsgReceived.data[0]);
+        Serial.print("Order received: ");
+        Serial.print(servoOrder ? "OPEN" : "CLOSE");
+        Serial.println(" BARRIER");
+      } 
+ 
+    }
+  }
+
+  servoAngle = servoOrder ? 90 : 0;
+  servoMotor.write(servoAngle);
+  if (verbose) {
+    Serial.print("Data sent to motor: servoAngle = ");
+    Serial.print(servoAngle);
+    Serial.println(" degrees.");
+  }
+
+  // Prepare CAN transmit message
+  canMsgSent.can_id  = SERVO_ID_OUT;
+  canMsgSent.can_dlc = 1;
+  canMsgSent.data[0] = (byte) (servoOrder & 0xFF);        // LSB
+
+  if (mcp2515.sendMessage(&canMsgSent) == MCP2515::ERROR_OK) {
+      Serial.print("[CAN] Sent degrees: ");
+      Serial.print(servoOrder, 1);
+      Serial.println(" degrees.");
+
+  }else{
+    Serial.print("[CAN] Error sending message in sensor ");
+    Serial.println(SERVO_ID_OUT);
+  }
+  delay(100);
+}
---
+++ industrialNetwork/transmitterCAN/ambient_transmitter.ino
@@ -0,0 +1,110 @@
+#include <SPI.h>
+//#include <mcp_can.h>
+#include <mcp2515.h>
+#include "Air_Quality_Sensor.h"
+
+#define CAN_CS 5  // Chip Select pin
+struct can_frame canTx;
+struct MCP2515 mcp2515(5); // CS pin is GPIO 5
+
+#define GAS_SENSOR_PIN 34  // Gas sensor pin
+#define AIR_QUALITY_PIN 35  // Air Quality sensor pin
+#define GAS_SENSOR_ID 0x610
+#define AIR_QUALITY_SENSOR_ID 0x520
+
+#define LOOP_DELAY 5000
+
+AirQualitySensor aq_sensor(AIR_QUALITY_PIN);
+
+void setup() {
+  Serial.begin(115200);
+
+  SPI.begin();
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+
+  
+  // Initializing Air Quality Sensor
+  Serial.println("Waiting for air quality sensor to init...");
+  // delay(20000); // we'll comment that for now
+  Serial.println("Waiting sensor to init...");
+  delay(10000);
+
+  if (aq_sensor.init()) {
+      Serial.println("Sensor ready.");
+  } else {
+      Serial.println("Sensor ERROR!");
+  }
+
+  Serial.println("Air sensor ready.");
+
+}
+
+void loop() {
+  int gasSensorValue;
+  int gasSensorVolt;
+  int verbose = 1;
+  byte sndStat;
+
+
+  // Gas sensor reading
+  gasSensorValue = analogRead(GAS_SENSOR_PIN);
+  gasSensorVolt = (int) (gasSensorValue/4096.0 * 5.0 * 100.0); 
+  
+  delay(500);
+
+  // Air quality sensor reading
+  int quality = aq_sensor.slope();
+  int airQualitySensorValue = aq_sensor.getValue();
+  if (verbose){
+    Serial.print("Sensor value: ");
+    Serial.println(airQualitySensorValue);
+    if (quality == AirQualitySensor::FORCE_SIGNAL) {
+        Serial.println("High pollution! Force signal active.");
+    } else if (quality == AirQualitySensor::HIGH_POLLUTION) {
+        Serial.println("High pollution!");
+    } else if (quality == AirQualitySensor::LOW_POLLUTION) {
+        Serial.println("Low pollution!");
+    } else if (quality == AirQualitySensor::FRESH_AIR) {
+        Serial.println("Fresh air.");
+    }
+  }
+
+  delay(500);
+
+
+  // Prepare CAN transmit message from gas sensor
+  canTx.can_id  = GAS_SENSOR_ID;
+  canTx.can_dlc = 2;
+  canTx.data[0] = (byte) ((gasSensorVolt >> 8) & 0xFF); // MSB
+  canTx.data[1] = (byte) (gasSensorVolt & 0xFF);        // LSB
+
+  if (mcp2515.sendMessage(&canTx) == MCP2515::ERROR_OK) {
+      Serial.print("[CAN] Sent gas sensor reading: ");
+      Serial.println(gasSensorVolt);
+  }else{
+    Serial.print("[CAN] Error sending message in gas sensor ");
+    Serial.println(GAS_SENSOR_ID);
+  }
+
+  delay(200);
+
+  // Prepare CAN transmit message from air quality sensor
+  canTx.can_id  = AIR_QUALITY_SENSOR_ID;
+  canTx.can_dlc = 2;
+  canTx.data[0] = (byte) ((airQualitySensorValue >> 8) & 0xFF); // MSB
+  canTx.data[1] = (byte) (airQualitySensorValue & 0xFF);        // LSB
+
+  if (mcp2515.sendMessage(&canTx) == MCP2515::ERROR_OK) {
+      Serial.print("[CAN] Sent air quality: ");
+      Serial.println(airQualitySensorValue);
+
+  }else{
+    Serial.print("[CAN] Error sending message in air quality sensor ");
+    Serial.println(AIR_QUALITY_SENSOR_ID);
+  }
+
+  delay(LOOP_DELAY);
+}
